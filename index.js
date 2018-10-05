95e4093f9770caa90b176fb0290b515609b54c44
WheatleyTheCore <alex.colwell@icloud.com>
2018-10-04 21:49:35 -0400

    added comments and deleted empty file

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var fs = require('fs')
-var chalk = require('chalk')
+var fs = require('fs') //lets us access and read files
+var chalk = require('chalk') //lets us color the output
 
 
 fs.readFile('./data.txt', 'utf8', (err, data) => {
-    let content = data.split(' ');
-    let color = content[0];
-    let txt = content[1];
-    console.log(chalk[color](txt));
+    let content = data.split(' '); //makes an array of all the words of data.txt
+    let color = content[0]; //makes a variable called color that has the value of the first word in data.txr
+    let txt = content[1]; //makes a variable called txt with the value of the second word in data.txt
+    console.log(chalk[color](txt)); //prints the second word in data.txt colored as the first word in data.txt on the next line of the console
 })
